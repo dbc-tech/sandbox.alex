d355f44f8f560db451cb558cf03a6528a930af1f
Alex Ringor <alexander.ringor@conveyancing.com.au>
2023-10-23 09:23:03 +0800

    VueRouter

---
+++ VueRouter/vue-router-vite-only/vue-router-school-travel/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+import TheNavigation from './components/TheNavigation.vue';
+export default {
+  components: {TheNavigation}
+}
+</script>
+
+<template>
+  <div>
+    <TheNavigation/>
+      <div class="container">
+        <router-view></router-view>
+      </div>
+  </div>
+</template>
